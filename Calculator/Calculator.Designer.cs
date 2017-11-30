8e2a22a7c8175c19f7340f5c8623330e54276fda
Rutulpatel7077 <rutulpatel7077@gmail.com>
2017-11-30 14:15:55 -0500

    first commit

---
+++ Calculator/Calculator.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace Calculator
+{
+    partial class frmCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCalculator));
+            this.printForm1 = new Microsoft.VisualBasic.PowerPacks.Printing.PrintForm(this.components);
+            this.lblRutul = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFirstNumber = new System.Windows.Forms.TextBox();
+            this.txtSecondNumber = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.cboOperations = new System.Windows.Forms.ComboBox();
+            this.btnResult = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // printForm1
+            // 
+            this.printForm1.DocumentName = "document";
+            this.printForm1.Form = this;
+            this.printForm1.PrintAction = System.Drawing.Printing.PrintAction.PrintToPrinter;
+            this.printForm1.PrinterSettings = ((System.Drawing.Printing.PrinterSettings)(resources.GetObject("printForm1.PrinterSettings")));
+            this.printForm1.PrintFileName = null;
+            // 
+            // lblRutul
+            // 
+            this.lblRutul.AutoSize = true;
+            this.lblRutul.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRutul.Location = new System.Drawing.Point(363, 345);
+            this.lblRutul.Name = "lblRutul";
+            this.lblRutul.Size = new System.Drawing.Size(110, 24);
+            this.lblRutul.TabIndex = 0;
+            this.lblRutul.Text = "Rutul Patel";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(177, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calculator";
+            // 
+            // txtFirstNumber
+            // 
+            this.txtFirstNumber.Location = new System.Drawing.Point(106, 88);
+            this.txtFirstNumber.Name = "txtFirstNumber";
+            this.txtFirstNumber.Size = new System.Drawing.Size(111, 20);
+            this.txtFirstNumber.TabIndex = 1;
+            // 
+            // txtSecondNumber
+            // 
+            this.txtSecondNumber.Location = new System.Drawing.Point(108, 135);
+            this.txtSecondNumber.Name = "txtSecondNumber";
+            this.txtSecondNumber.Size = new System.Drawing.Size(109, 20);
+            this.txtSecondNumber.TabIndex = 2;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(101, 224);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(367, 20);
+            this.txtResult.TabIndex = 7;
+            // 
+            // cboOperations
+            // 
+            this.cboOperations.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboOperations.FormattingEnabled = true;
+            this.cboOperations.Items.AddRange(new object[] {
+            "Average",
+            "Larger",
+            "Smaller",
+            "Remainder"});
+            this.cboOperations.Location = new System.Drawing.Point(295, 87);
+            this.cboOperations.Name = "cboOperations";
+            this.cboOperations.Size = new System.Drawing.Size(147, 21);
+            this.cboOperations.TabIndex = 3;
+            // 
+            // btnResult
+            // 
+            this.btnResult.Location = new System.Drawing.Point(52, 300);
+            this.btnResult.Name = "btnResult";
+            this.btnResult.Size = new System.Drawing.Size(83, 40);
+            this.btnResult.TabIndex = 4;
+            this.btnResult.Text = "&Result";
+            this.btnResult.UseVisualStyleBackColor = true;
+            this.btnResult.Click += new System.EventHandler(this.btnResult_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(162, 300);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(83, 39);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "C&lear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(330, 302);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(84, 38);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "&First Number :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "S&econd Number :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(292, 71);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "&Operation";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(49, 227);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "O&utPut :";
+            // 
+            // frmCalculator
+            // 
+            this.AcceptButton = this.btnResult;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 378);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnResult);
+            this.Controls.Add(this.cboOperations);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtSecondNumber);
+            this.Controls.Add(this.txtFirstNumber);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblRutul);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmCalculator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Microsoft.VisualBasic.PowerPacks.Printing.PrintForm printForm1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnResult;
+        private System.Windows.Forms.ComboBox cboOperations;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.TextBox txtSecondNumber;
+        private System.Windows.Forms.TextBox txtFirstNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblRutul;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Calculator/Calculator.cs
@@ -0,0 +1,187 @@
+﻿/*
+ * Name: Rutul Patel
+ * Date: 30th November
+ * Student Number : 200335158
+ * Purpose: Inputs for the basic calculator to count and calculate 
+ * Average, Compare numbers in between and find remainder of those numbers.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class frmCalculator : Form
+    {
+        public frmCalculator()
+        {
+            InitializeComponent();
+        }
+
+        // Decalring 2 required variables for the operations
+        decimal number1;
+        decimal number2;
+
+
+
+        /// <summary>
+        /// This method will processed when Result button clicked 
+        /// </summary>
+        /// <param name="sender">Result Button</param>
+        /// <param name="e">The Event</param>
+        private void btnResult_Click(object sender, EventArgs e)
+        {
+
+            //if Operation is not selected
+            if (cboOperations.SelectedItem == null)
+            {
+                MessageBox.Show("You have to select at least one Operation", "Missing Operation", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                cboOperations.Focus();
+            }
+
+            //take the number1 input and parse it as decimal
+            try
+            {
+                number1 = decimal.Parse(txtFirstNumber.Text.Trim());
+
+                //take the number2 input and parse it as decimal
+                try
+                {
+                    number2 = decimal.Parse(txtSecondNumber.Text.Trim());
+
+                    // Swith case with Combo box with four different required cases
+                    switch (cboOperations.SelectedIndex)
+                    {
+                        // case 0: is for Average of number 1 and number 2
+                        // Printing all the number value with 2 decimal value 
+
+
+                        case 0: txtResult.Text = "The average of " + number1.ToString("F") + " and " + number2.ToString("F") + "  is " +( (number1 + number2) / 2).ToString("F"); break;
+
+                        //Case 1 : is for Greater operations
+                        case 1:
+                            if (number1 > number2)
+                            {
+                                txtResult.Text = "First number " + number1.ToString("F") + " is greater than Second Number  " + number2.ToString("F"); break;
+                            }
+                            else if (number2 > number1)
+                            {
+                                txtResult.Text = "Second Number " + number2.ToString("F") + " is greater than First Number " + number1.ToString("F"); break;
+                            }
+                            else
+                            {
+                                txtResult.Text = "First number "+number1.ToString("F") + " and " + "Second Number "+number2.ToString("F") + " is equal";
+                            }
+                            break;
+                        // Case 2: is for smaller operations
+                        case 2:
+
+                            if (number1 < number2)
+                            {
+                                txtResult.Text = "First number " + number1.ToString("F") + " is smaller than Second Number  " + number2.ToString("F"); break;
+                            }
+                            else if (number2 < number1)
+                            {
+                                txtResult.Text = "Second Number " + number2.ToString("F") + " is smaller than First Number " + number1.ToString("F"); break;
+                            }
+                            else
+                            {
+                                txtResult.Text = "First number" + number1.ToString("F") + " and Second number " + number2.ToString("F") + " is equal";
+                            }
+                            break;
+
+                        // Case 3 is for Remainder 
+                        case 3:
+                            //if remainder is not possible or if remainder is 0 then throw error
+                            if ((number1 % number2) == 0)
+                            {
+                                MessageBox.Show("Your inputs Remainder is zero(0). Please change any number to count remainder", "Input error",
+                               MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                txtSecondNumber.SelectAll();
+                                txtSecondNumber.Focus();
+                            }
+
+                            // show the result with remainder
+                            else
+                            {
+                                txtResult.Text = "we have" + number1.ToString("F") + " and " + number2.ToString("F") +  " and Remainder " + (number1 % number2).ToString("F");
+                            }
+                            break;
+                    } //end of switch 
+
+                } // end of second Try
+
+                catch (FormatException inputFe)
+                {
+
+                    //if second number textbox is empty print empty message
+                    if (txtSecondNumber.Text == String.Empty)
+                    {
+                        MessageBox.Show("Second Number is missing", "Missing Number", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        txtSecondNumber.Focus();
+                    }
+
+                    // if number second number is not an Integer show error
+                    else
+                    {
+                        MessageBox.Show("Your inputs must be a Real Numbers. Please check your Second Number", "Input error",
+                         MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        txtSecondNumber.SelectAll();
+                        txtSecondNumber.Focus();
+                    }
+                } //end of second catch
+
+            } //end of first try
+
+            catch (FormatException inputFe)
+            {
+                //if first number text box is empty print empty message
+                if (txtFirstNumber.Text == String.Empty)
+                {
+                    MessageBox.Show("First Number is Missing", "Missing Number", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txtFirstNumber.Focus();
+                }
+                else
+                {
+                    MessageBox.Show("Your inputs must be a Real Numbers. Please check your First Number", "Input error",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txtFirstNumber.SelectAll();
+                    txtFirstNumber.Focus();
+                }
+            } //end of first catch 
+        } // end of result button method
+
+        /// <summary>
+        /// This method will clear all the text boxes of number 1 and number 2
+        /// </summary>
+        /// <param name="sender">Clear Button</param>
+        /// <param name="e"> Action event</param>
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtFirstNumber.Clear();
+            txtSecondNumber.Clear();
+            txtResult.Clear();
+            cboOperations.SelectedItem.Equals(null);
+
+        }
+
+        /// <summary>
+        /// This button will exit the programme
+        /// </summary>
+        /// <param name="sender">Exit Button</param>
+        /// <param name="e">Action Event</param>
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }//end of class
+
+}
+
